6369ee4938d31d85f6429ab4207407a8eaf46394
Gareth Hind <garethjhind@gmail.com>
2022-10-07 21:53:09 +1100

    CRUD

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express();
 
@@ -23,6 +26,9 @@ mongoose.connection.on("disconnected", () => {
 // Middlewares
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.listen(8800, () => {
   connect();
---
+++ api/routes/hotels.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+// TODO - CREATE
+// TODO - UPDATE
+// TODO - DELETE
+// TODO - GET
+// TODO - GET ALL
+
+export default router;

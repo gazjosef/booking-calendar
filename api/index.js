f3290a9fb523f45b2fa679ac4335b460f6acd521
Gareth Hind <garethjhind@gmail.com>
2022-10-07 21:47:27 +1100

    Create First API

---
+++ api/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+
+const app = express();
+
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
+
+// Middlewares
+
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend!");
+});

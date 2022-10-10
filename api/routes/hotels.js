28c23eae5d2435459a3d9966e039af8fe4b1d48c
Gareth Hind <garethjhind@gmail.com>
2022-10-10 17:39:48 +1100

    Fixed Bug - Router Post Callback

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,21 +1,20 @@
 import express from "express";
-import Hotel from "../models/Hotel.js";
+import Hotel from "../models/Hotel.js ";
 
 const router = express.Router();
 
 // TODO - CREATE
 
-router.post("/"),
-  async (req, res) => {
-    const newHotel = new Hotel(req.body);
+router.post("/", async (req, res) => {
+  const newHotel = new Hotel(req.body);
 
-    try {
-      const savedHotel = await newHotel.save();
-      res.status(200).json(savedHotel);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  };
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // TODO - UPDATE
 // TODO - DELETE

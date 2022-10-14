73fe6a7034659e6b0390b7fa71a1989b42c6dac9
Gareth Hind <garethjhind@gmail.com>
2022-10-14 22:20:02 +1100

    Verification

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,13 +1,27 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
-// router.get("/", (req, res) => {
-//   res.send("Hello this is auth endpoint");
-// });
+// * UPDATE
 
-// router.get("/register", (req, res) => {
-//   res.send("Hello this is auth register endpoint");
-// });
+router.put("/:id", updateUser);
+
+// * DELETE
+
+router.delete("/:id", deleteUser);
+
+// * GET
+
+router.get("/:id", getUser);
+
+// * GET ALL
+
+router.get("/", getUsers);
 
 export default router;

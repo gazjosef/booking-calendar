b7dfe5056f32a37ce9952dda48a4f467c5dfb794
Gareth Hind <garethjhind@gmail.com>
2023-03-11 14:18:15 +1100

    Updated

--- app-ui-tw/components/Header.jsx
+++ app-ui-tw/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="p-6 shadow-[0_0_10px_0_rgba(0,0,0,0.8)]">
       <div className="| container mx-auto flex items-center justify-between">
-        <div className="head__logo">LOGO</div>
+        <div className="head__logo">CALENDAR</div>
         <div className="head__login">LOGIN</div>
       </div>
     </header>

1a0c0c540414249c4963d0d3130a9c36fb665ccd
Gareth Hind <garethjhind@gmail.com>
2023-04-20 17:57:29 +1000

    Updated

--- app/.next/static/webpack/webpack.54982ef75dcd1ca9.hot-update.js
+++ app/.next/static/webpack/webpack.54982ef75dcd1ca9.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1fcec1129bca32fc"; }
+/******/ 	__webpack_require__.h = function() { return "8d1642472773868a"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );

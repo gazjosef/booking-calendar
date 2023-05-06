95911588bc36d05be23652f86188d5e6dfe890e7
Gareth Hind <garethjhind@gmail.com>
2023-05-06 14:51:42 +1000

    Updated roomController.js

---
+++ backend/controller/roomController.js
@@ -0,0 +1,54 @@
+const asyncHandler = require("express-async-handler");
+
+// *** GET ROOM
+// @route   GET /api/rooms
+// @access  Private
+const getRoom = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: `Get Room ${req.params.id}`,
+  });
+});
+
+// *** GET ALL ROOMS
+// @route   GET /api/rooms
+// @access  Private
+const getAllRooms = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: "Get All Rooms",
+  });
+});
+
+// *** SET ROOM
+// @route   POST /api/rooms
+// @access  Private
+const setRoom = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: "Set Room",
+  });
+});
+
+// *** UPDATE ROOM
+// @route   PUT /api/rooms/:id
+// @access  Private
+const updateRoom = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: `Update Room ${req.params.id}`,
+  });
+});
+
+// *** DELETE ROOM
+// @route   DELETE /api/rooms/:id
+// @access  Private
+const deleteRoom = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: `Delete Room ${req.params.id}`,
+  });
+});
+
+module.exports = {
+  getRoom,
+  getAllRooms,
+  setRoom,
+  updateRoom,
+  deleteRoom,
+};
--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getRoom,
+  getAllRooms,
+  setRoom,
+  updateRoom,
+  deleteRoom,
+} = require("../controller/roomController");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get Rooms" });
-});
+router.route("/").get(getAllRooms).post(setRoom);
 
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Book Room" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update Room ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete Room ${req.params.id}` });
-});
+router.route("/:id").get(getRoom).put(updateRoom).delete(deleteRoom);
 
 module.exports = router;

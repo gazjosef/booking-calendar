8be6b6401d38692f883198cfe15f9dec067a41ff
Gareth Hind <garethjhind@gmail.com>
2023-05-08 20:05:55 +1000

    Protecting Routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,9 +5,10 @@ const {
   loginUser,
   getMe,
 } = require("../controller/userController.js");
+const { protect } = require("../middleware/authMiddleware.js");
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/me", getMe);
+router.get("/me", protect, getMe);
 
 module.exports = router;

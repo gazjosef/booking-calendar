175b4a40bea8cf122ffc68230007437575fd32c3
Gareth Hind <garethjhind@gmail.com>
2023-05-08 18:14:48 +1000

    Auth Middleware

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -72,7 +72,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // *** GET USER DATA
 // @route   GET /api/users/me
-// @access  Public
+// @access  Private
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data display" });
 });

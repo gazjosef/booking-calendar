38c0e677d62eef73b5eaee6a9a4ceea8c594eaa6
Gareth Hind <garethjhind@gmail.com>
2023-05-08 17:10:41 +1000

    Authenticate User

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -51,7 +51,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login User" });
+  const { email, password } = req.body;
+
+  // CHECK FOR USER EMAIL
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 // *** GET USER DATA
